7d43c7ba03e5f35082dd9f87ccd78e9989403c28
sammy2511 <42390656+sammy2511@users.noreply.github.com>
2018-09-10 22:45:00 +0530

    Code refractoring

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User',{
+  email:{
+    required:true,
+    type:String,
+    minlength:1,
+    trim:true
+  }
+});
+
+module.exports = {User}
---
+++ server/server.js
@@ -0,0 +1,4 @@
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./db/models/todo');
+var { User } = require('./db/models/user');
+const express = require('express');
