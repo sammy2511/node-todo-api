e7676e57e67b0c9c5c5ebc5cb7a7ddf49529a44e
sammy2511 <42390656+sammy2511@users.noreply.github.com>
2018-09-10 23:10:17 +0530

    added start script

--- server/server.js
+++ server/server.js
@@ -1,4 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./db/models/todo');
-var { User } = require('./db/models/user');
-const express = require('express');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+var app = express();
+
+//middleware
+app.use(bodyParser.json());
+
+
+//route to POST
+app.post('/todos',(req,res) => {
+  console.log(req.body);
+})
+
+
+
+//starting server
+app.listen(3000,() => {
+  console.log('Server started on port 3000');
+})
