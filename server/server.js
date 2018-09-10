43ae337d122d06d3762a23f6d0e351625e58d8fd
sammy2511 <42390656+sammy2511@users.noreply.github.com>
2018-09-11 00:08:48 +0530

    added GET Todos by Id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -32,6 +32,28 @@ app.get('/todos',(req,res) => {
     res.send({todos});
   },(err) => {
     res.status(400).send(err);
+  });
+});
+
+//route to GET todos by ID
+app.get('/todos/:id',(req,res) => {
+  var id = req.params.id;
+
+  //if not Valid ID
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  //find by ID
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      res.send(todo);
+    }
+    else{
+      res.status(404).send();
+    }
+  },(e) => {
+    res.status(400).send();
   });
 });
 
