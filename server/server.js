6dfc84a12887bdb4aecb98bd8938c27e029e4c20
sammy2511 <42390656+sammy2511@users.noreply.github.com>
2018-09-28 23:13:24 +0530

    Added Patch Endpoint

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const {ObjectID} = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+const lodash = require('lodash');
 const port = process.env.PORT || 3000;
 var app = express();
 
@@ -78,6 +79,34 @@ app.delete('/todos/:id',(req,res) => {
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+//Update todo
+app.patch('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  var body = lodash.pick(req.body,['text','completed']);
+
+  //if not Valid ID
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(lodash.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch(() => {
+    res.status(400).send();
+  });
+
 });
 
 
