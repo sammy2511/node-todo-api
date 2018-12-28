81e34403505bcc554f0abe59880168348e43789b
sammy2511 <42390656+sammy2511@users.noreply.github.com>
2018-12-28 10:52:29 +0530

    Update mongoose.js
    Added comment for promise

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
+
+//latest versions of mongoose doesn't required global promiseo
 mongoose.Promise = global.Promise;
+
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose }
